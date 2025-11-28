baa28ee47960ff132262e18e5285e2b96a366759
Joris Lodewijks <info@jorislodewijks.nl>
2025-11-28 22:41:40 +0100

    fix: update API proxy target to production URL

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api/v1": {
-        // target: "https://counting-down-fastapi-2ad079e42b08.herokuapp.com/",
-        target: "http://localhost:8000",
+        target: "https://counting-down-fastapi-2ad079e42b08.herokuapp.com/",
+        // target: "http://localhost:8000",
         changeOrigin: false,
       },
     },
